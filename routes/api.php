797ab56b1e2bdcdd30e066f78bddfc199812bffd
Alexander <alexandr_gon@bk.ru>
2025-09-03 22:26:47 +0700

    init

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+
+    'middleware' => 'auth:api',
+
+], function ($router) {
+
+    Route::post('logout', [AuthController::class,'logout    ']);
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', [AuthController::class,'me']);
+    
+});
+Route::post('login', [AuthController::class,'login']);
+Route::post('register', [AuthController::class,'register']);
+Route::get('test',function(){
+    return response()->json(['popa'=>'chlen']);
+});
